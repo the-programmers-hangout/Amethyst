c2ac52a5caf31399741dd358c4ac77950a3cebff
Elliott Short <elliott.short@gmail.com>
2019-02-24 17:30:36 -0600

    Script state management PoC

--- src/main/kotlin/me/elliott/amethyst/commands/JSCommands.kt
+++ src/main/kotlin/me/elliott/amethyst/commands/JSCommands.kt
@@ -1,14 +1,18 @@
 package me.elliott.amethyst.commands
 
 import jdk.nashorn.api.scripting.NashornScriptEngineFactory
+import me.aberrantfox.kjdautils.api.dsl.CommandEvent
 import me.aberrantfox.kjdautils.api.dsl.CommandSet
 import me.aberrantfox.kjdautils.api.dsl.CommandsContainer
 import me.aberrantfox.kjdautils.api.dsl.commands
 import me.aberrantfox.kjdautils.internal.command.arguments.SentenceArg
+import me.elliott.amethyst.services.ScriptEngineService
 import net.dv8tion.jda.core.JDA
 import java.io.File
 import javax.script.Invocable
 import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
+import javax.script.ScriptException
 
 object EngineContainer {
     var engine: ScriptEngine? = null
@@ -42,8 +46,7 @@ fun jsCommands() = commands {
             val functionContext = createFunctionContext(script)
 
             try {
-                EngineContainer.engine?.eval(functionContext)
-                (EngineContainer.engine as Invocable).invokeFunction(functionName, it)
+                execWithThread(EngineContainer.engine, script, it)
             } catch (e: Exception) {
                 it.respond("${e.message} - **cause** - ${e.cause}")
             }
@@ -57,3 +60,43 @@ private fun createFunctionContext(scriptBody: String) =
             $scriptBody
         };
     """.trimIndent()
+
+private fun execWithThread(
+        engine: ScriptEngine?, script: String, commandEvent: CommandEvent) {
+    val functionContext = createFunctionContext(script)
+    val r = Runnable {
+        try {
+            engine?.eval(functionContext)
+            (EngineContainer.engine as Invocable).invokeFunction(functionName, commandEvent)
+
+        } catch (e: ScriptException) {
+            println(
+                    "Java: Caught exception from eval(): " + e.message)
+        }
+    }
+    println("Java: Starting thread...")
+    val t = Thread(r)
+    t.start()
+    println("Java: ...thread started")
+
+    val threads = mutableMapOf<String, Thread>()
+    threads["whileTrue"] = t
+
+    Thread.sleep(5_000)
+
+    threads["whileTrue"]?.stop()
+    print("stopped")
+
+
+//    try {
+//        if (t.isAlive) {
+//            println("Java: Thread alive after timeout, stopping...")
+//            t.stop()
+//            println("Java: ...thread stopped")
+//        } else {
+//            println("Java: Thread not alive after timeout.")
+//        }
+//    } catch (e: InterruptedException) {
+//        println("Interrupted while waiting for timeout to elapse.")
+//    }
+}
